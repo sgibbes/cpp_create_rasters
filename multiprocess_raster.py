c9b5cb00ebc04cebb0b14146150d2679018098bc
Ubuntu <charlie.hofmann@gmail.com>
2017-04-10 13:27:45 -0400

    file names

--- multiprocess_raster.py
+++ multiprocess_raster.py
@@ -2,22 +2,22 @@ import multiprocessing
 import time
 import subprocess
 
-tile_list = ['00N_000E', '00N_010E', '00N_020E', '00N_030E', '00N_040E']
+tile_list = ['00N_010E', '00N_020E', '00N_030E', '00N_040E']
 
 def process_tile(tile_id):
 
     print 'downloading loss and extent for tile id {}'.format(tile_id)
     time.sleep(1)
-    extent_url = r'http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2014/Hansen_GFC2014_treecover2000_{}.tif'.format(tile_name)
-    loss_url = r'http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2014/Hansen_GFC2014_lossyear_{}.tif'.format(tile_name)
-    extent_local = r'data/{}_extent.tif'.format(tile_name)
-    loss_local = r'data/{}_loss.tif'.format(tile_name)
+    extent_url = r'http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2014/Hansen_GFC2014_treecover2000_{}.tif'.format(tile_id)
+    loss_url = r'http://commondatastorage.googleapis.com/earthenginepartners-hansen/GFC2014/Hansen_GFC2014_lossyear_{}.tif'.format(tile_id)
+    extent_local = r'data/{}_extent.tif'.format(tile_id)
+    loss_local = r'data/{}_loss.tif'.format(tile_id)
 
     subprocess.check_call(['wget', '-O', extent_local, extent_url])
     subprocess.check_call(['wget', '-O', loss_local, loss_url])
     
     print 'writing combined tile for {}'.format(tile_id)
-    encoded_tile = '{}_losstcd.tif'
+    encoded_tile = '{}_losstcd.tif'.format(tile_id)
     encode_tiles_cmd = ['./encode_hansen_loss_tcd.exe', extent_local, loss_local, encoded_tile]
     subprocess.check_call(encode_tiles_cmd)
     
@@ -28,5 +28,5 @@ def process_tile(tile_id):
 
 if __name__ == '__main__':
     count = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=count)
+    pool = multiprocessing.Pool(processes=1)
     pool.map(process_tile, tile_list)
